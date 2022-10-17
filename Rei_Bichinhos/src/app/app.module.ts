420636227845e4954b624f6facc250ebf90f4100
ardl <ardl@gft.com>
2022-10-17 13:41:13 -0300

    Navbar and footer

--- Rei_Bichinhos/src/app/app.module.ts
+++ Rei_Bichinhos/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { AreaAdministrativaComponent } from './navegacao/area-administrativa/area-administrativa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    AreaAdministrativaComponent
   ],
   imports: [
     BrowserModule,
---
+++ Rei_Bichinhos/src/app/navegacao/area-administrativa/area-administrativa.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-administrativa',
+  template: `
+    <p>
+      area-administrativa works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AreaAdministrativaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
