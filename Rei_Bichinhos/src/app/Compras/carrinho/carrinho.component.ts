baecbb00bab9e2cac9049191a9a4b8213e3318a2
ardl <ardl@gft.com>
2022-11-26 23:20:33 -0300

    Carrinho de compras e correcao do layout

--- Rei_Bichinhos/src/app/Compras/carrinho/carrinho.component.ts
+++ Rei_Bichinhos/src/app/Compras/carrinho/carrinho.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/Interface/Produto';
+import { ProdutosService } from 'src/app/Service/produtos.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-carrinho',
@@ -8,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrinhoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private produtosService: ProdutosService, private ActivatedRoute: ActivatedRoute) { }
+  public "produtos": Produto[];
+  public produto: Produto = {} as Produto;
+
+
+  public getProdutos() {
+    this.produtosService.getProdutos()
+      .subscribe(
+        produto => {
+          this.produtos = produto
+          console.log(produto)
+        },
+        error => { console.log(error) }
+      )
+
+  }
+
+  getProdutoId() {
+    this.produtosService.getProdutoById(this.ActivatedRoute.snapshot.paramMap.get('id')).subscribe(
+      produto => {
+        this.produto = { ...produto } as Produto
+        console.log(JSON.stringify(this.produto))
+      },
+      erro => {
+
+        console.log('Não foi possivel localizar o Produto')
+      }
+    )
+  }
+
 
   ngOnInit(): void {
+    // this.getProdutos();
+    this.getProdutoId();
   }
 
 }
